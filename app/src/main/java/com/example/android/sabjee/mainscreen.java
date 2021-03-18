09fef587ff6a14fcc8a979785c2d2666dd93c633
ayushsina <jnpayush98@gmail.com>
2021-03-18 23:37:38 +0530

    mainscreen implemnted with cart button and account info...

--- app/src/main/java/com/example/android/sabjee/mainscreen.java
+++ app/src/main/java/com/example/android/sabjee/mainscreen.java
@@ -1,10 +1,12 @@
 package com.example.android.sabjee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.animation.GridLayoutAnimationController;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,13 +21,26 @@ public class mainscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
         recview=(RecyclerView)findViewById(R.id.recview);
-        recview.setLayoutManager(new LinearLayoutManager(this));
+//        recview.setLayoutManager(new LinearLayoutManager(this));
+
         FirebaseRecyclerOptions<model> options =
                 new FirebaseRecyclerOptions.Builder<model>()
                         .setQuery(FirebaseDatabase.getInstance().getReference("vegetable"), model.class)
                         .build();
         adapter=new myadapter(options);
         recview.setAdapter(adapter);
+        /* code for grid vie
+
+         */
+
+//        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+//        recview.setLayoutManager(layoutManager);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
+        recview.setLayoutManager(gridLayoutManager);
+
+
+
+
     }
     @Override
     protected void onStart() {
